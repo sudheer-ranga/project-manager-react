148c372c1dd6a22c8dd3b445966053ad1d6307ee
Sudheer Ranga <sudheer.ranga1@gmail.com>
2020-05-11 23:28:45 +0530

    Updated mock data

--- src/redux/mock.js
+++ src/redux/mock.js
@@ -1,6 +1,103 @@
 const boards = {
   boards: [
     {
+      name: "Unacademy",
+      id: "wersafuejj1u7583",
+      data: [
+        {
+          id: "asasdf12j124j",
+          name: "Design",
+          cards: [
+            {
+              id: "asdfj23j243",
+              text:
+                '1. This is a card. Drag it on to "tried it" to show it\'s done.'
+            },
+            {
+              id: "asdfj23j244",
+              text:
+                '2. This is a card. Drag it on to "tried it" to show it\'s done.'
+            },
+            {
+              id: "asdfj23j245",
+              text:
+                '3. This is a card. Drag it on to "tried it" to show it\'s done.'
+            },
+            {
+              id: "asdfj23j246",
+              text:
+                '4. This is a card. Drag it on to "tried it" to show it\'s done.'
+            }
+          ]
+        },
+        {
+          id: "rasdweafea243s2",
+          name: "Todo",
+          cards: [
+            {
+              id: "asdfj23j247",
+              text:
+                '5. This is a card. Drag it on to "tried it" to show it\'s done.'
+            },
+            {
+              id: "asdfj23j248",
+              text:
+                '6. This is a card. Drag it on to "tried it" to show it\'s done.'
+            }
+          ]
+        },
+        {
+          id: "werasrasdweafea243s2",
+          name: "Development",
+          cards: [
+            {
+              id: "asdfj23j247",
+              text:
+                '5. This is a card. Drag it on to "tried it" to show it\'s done.'
+            }
+          ]
+        },
+        {
+          id: "readfeaweeasdweafea243s2",
+          name: "QA",
+          cards: [
+            {
+              id: "asdfj23j247",
+              text:
+                '5. This is a card. Drag it on to "tried it" to show it\'s done.'
+            },
+            {
+              id: "asdfj23j248",
+              text:
+                '6. This is a card. Drag it on to "tried it" to show it\'s done.'
+            }
+          ]
+        },
+        {
+          id: "qwersadfdrasdweafea243s2",
+          name: "Ready for release",
+          cards: [
+            {
+              id: "asdfj23j248",
+              text:
+                '6. This is a card. Drag it on to "tried it" to show it\'s done.'
+            }
+          ]
+        },
+        {
+          id: "weasdfdfwdfa43rwdafd34",
+          name: "Done",
+          cards: [
+            {
+              id: "asdfj23j247",
+              text:
+                '5. This is a card. Drag it on to "tried it" to show it\'s done.'
+            }
+          ]
+        }
+      ]
+    },
+    {
       name: "Welcome Board",
       id: "alsdfuejj1u7583",
       data: [
